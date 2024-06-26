3cc86b16fe27aa587a2b9dd7bf5f020a5d53ad20
Shubham Rathee <shubham.rathee@in2ittech.com>
2024-06-26 16:52:58 +0530

    change the generation type of id from sequence to itentity

--- domain/src/main/java/com/example/entity/Hotel.java
+++ domain/src/main/java/com/example/entity/Hotel.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 public class Hotel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String classification;
